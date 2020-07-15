38e3a871ec39c396b6eb8340932920341f893817
Heikki Hannula <heikki@gemming.fi>
2020-07-15 18:11:43 +0300

    Playoing with regex and dry

--- app/concepts/spade/spade_form.rb
+++ app/concepts/spade/spade_form.rb
@@ -7,7 +7,7 @@ class Spade::SpadeForm < Reform::Form
 
   validation do
     params do
-      required(:color).filled
+      required(:color).filled(:string)
       required(:width).filled(:integer)
       required(:length).filled(:integer)
     end
@@ -15,6 +15,10 @@ class Spade::SpadeForm < Reform::Form
     rule(:width) do
       key.failure('must be bigger than 50') if value <= 50
       key.failure('must be smaller than 150') if value >= 150
+    end
+
+    rule(:color) do
+      key.failure('must be html color like aff222 or aab') unless value.match(/^(?:[0-9a-fA-F]{3}){1,2}$/)
     end
 
     rule(:length) do
