99025f32790c263f6dc7cd7b736d0e273ac8606e
Heikki Hannula <heikki@gemming.fi>
2020-07-15 18:30:51 +0300

    Minor visuals

--- app/concepts/spade/cell/show.rb
+++ app/concepts/spade/cell/show.rb
@@ -4,8 +4,11 @@ module Spade::Cell
 
     private
 
-    def measures
-      "<div style=\"height: #{spade.length/5}px; width:#{spade.width/5}px; background: ##{spade.color};\"></div>"
+    def to_div
+      """
+      <div style=\"margin-top: #{spade.width/2}px; height: 4px; border: 1px solid black; background: ##{spade.color};\"></div>
+      <div style=\"margin-top: -#{spade.width/4}px; height: #{spade.width/2}px; width:#{spade.length/2}px; background: ##{spade.color};\"></div>
+      <div style=\"margin-top: -#{spade.width/2}px; float: right; height: #{spade.width/2 - 4}px; width:4px; background: black;\"></div>"""
     end
   end
 end
