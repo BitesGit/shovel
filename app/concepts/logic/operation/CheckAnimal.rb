ceb2da18e41022af5b8bf2fa11941d043a3a1e23
Heikki Hannula <heikki@gemming.fi>
2020-07-20 17:13:35 +0300

    Playing with wiring

--- app/concepts/logic/operation/CheckAnimal.rb
+++ app/concepts/logic/operation/CheckAnimal.rb
@@ -8,14 +8,25 @@ module Logic::Operation
 
   class CheckAnimal < Trailblazer::Activity::Railway
     Found = Class.new(Trailblazer::Activity::Signal)
+    Extinct = Class.new(Trailblazer::Activity::Signal)
     ExceptionSignal = Class.new(Trailblazer::Activity::Signal)
 
+    # Three paths:
+    # found -> sound
+    # failure -> silence
+    # success -> sound if
+    #
+    # + exception handler
     step :check_cow, Output(Found, :found) => End(:found)
     step :check_dog, Output(Found, :found) => End(:found)
+    # No bat task at all...
+    step :check_rex, Output(Extinct, :found) => End(:found)
 
     step Rescue( Exception, handler: StoneExceptionHandler ) {
-      step :check_stone, Output(ExceptionSignal, :found) => End(:exception)
+      step :check_stone
     }
+
+    step :check_if_found
     fail :silence
 
     def silence(ctx, params:, **)
@@ -27,7 +38,7 @@ module Logic::Operation
         ctx[:sound] = 'Moo'
         return Found
       end
-      true
+      true # nothing unexpected happened...
     end
 
     def check_dog(ctx, params:, **)
@@ -35,14 +46,26 @@ module Logic::Operation
         ctx[:sound] = params['that'].upcase
         return Found
       end
-      true
+      true # nothing unexpected happened...
+    end
+
+    def check_rex(ctx, params:, **)
+      if params['this'] == 'rex'
+        ctx[:sound] = params['that'].upcase
+        return Extinct
+      end
+      true # nothing unexpected happened...
+    end
+
+    def check_if_found(ctx, params:, **)
+      false # if not in 'Found' path, we failed...
     end
 
     def check_stone(ctx, params:, **)
       if params['this'] == 'stone'
         throw Exception.new
       end
-      true
+      true # nothing unexpected happened...
     end
 
   end
--- app/controllers/logic_controller.rb
+++ app/controllers/logic_controller.rb
@@ -3,11 +3,8 @@ class LogicController < ApplicationController
     # Activity call signature return value:
     # signal, [ctx] = Logic::Operation::CheckAnimal.(ctx)
     signal, result = Logic::Operation::CheckAnimal.(ctx)
-    if signal.to_h[:semantic] == :found
-      @result = result[0][:sound] # ctx is result[0]
-    else
-      @result = 'Not found'
-    end
+    @found = "Signal: #{signal.to_h[:semantic].capitalize}"
+    @result = result[0][:sound]
     unless result[0][:exception_class].nil?
       @exception = "Neatly handled an exception: #{result[0][:exception_class]}"
     end
